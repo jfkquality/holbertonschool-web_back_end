34f4ab7e69a68d4fdb2079749644449cf0c02981
jfkquality <jfkquality@gmail.com>
2020-10-22 00:20:35 +0000

    574. Task 5. Fix parameter type.

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ sum a list """
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ return string rep of n """
     return (sum(input_list))
