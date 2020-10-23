15e6f9b3384c2417eb774bb72c58e9a9ca2019e2
jfkquality <jfkquality@gmail.com>
2020-10-23 01:02:58 +0000

    576. T1. Bugfix: Annotate return List.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,9 +8,9 @@ from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float, None, None]:
+async def async_comprehension() -> List:
     """ Yield list of random numbers btwn 0-10 """
     result = []
     async for i in async_generator():
         result.append(i)
-    return(result)
+    return (result)
