7142cd2ebed3de1cc4db5f592a2c08a4e296faea
jfkquality <jfkquality@gmail.com>
2020-10-23 00:41:05 +0000

    576. T-0. Init commit.

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" 0. Async Generator """
+
+import asyncio
+import random
+
+
+async def async_generator():
+    """ Yield randmom number btwn 0-10 """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
