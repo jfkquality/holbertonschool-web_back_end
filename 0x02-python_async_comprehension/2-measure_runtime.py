80f056b711d131fcebc6e6412184f9a6473be48a
jfkquality <jfkquality@gmail.com>
2020-10-23 01:48:42 +0000

    576. T2. Init commit.

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 2. Run time for four parallel comprehensions  """
+
+import asyncio
+import random
+import time
+from typing import Generator, List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> List[float]:
+    """ Return total elapsed time """
+    start = time.perf_counter()
+    await asyncio.gather(async_comprehension(),
+                         async_comprehension(),
+                         async_comprehension())
+    return time.perf_counter() - start
