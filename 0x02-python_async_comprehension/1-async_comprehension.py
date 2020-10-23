216327673cc484a3b838f5ce5944114e40b11028
jfkquality <jfkquality@gmail.com>
2020-10-23 01:10:32 +0000

    576. T1. Bugfix: Annotate result List. Use async comprehension.

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,14 +3,12 @@
 
 import asyncio
 import random
-from typing import Generator
+from typing import Generator, List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[Generator[float, None, None]]:
+async def async_comprehension() -> List[float]:
     """ Yield list of random numbers btwn 0-10 """
-    result = []
-    async for i in async_generator():
-        result.append(i)
-    return (result)
+    result = [i async for i in async_generator() if i < 10]
+    return result
