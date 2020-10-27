0af42bced6e39c814af6611c80b3938095f42c62
jfkquality <jfkquality@gmail.com>
2020-10-27 04:05:09 +0000

    390. T.5/100. Init commit.

---
+++ 0x03-caching/100-lfu_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" 5/100. LFU caching """
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """ Basic Cache class """
+
+    def __init__(self):
+        """ Override superclass __init__ """
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Setter method """
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
+        # print("CACHE {}".format(self.cache_data))
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # last = list(self.cache_data.keys())[-1]
+            last = list(self.cache_data.keys())[0]
+            self.cache_data.pop(last)
+            # last = self.cache_data.popitem(last=False)
+            print("DISCARD: {}".format(last[0]))
+
+    def get(self, key):
+        """ Getter method """
+        if key in self.cache_data and key is not None:
+            self.cache_data.move_to_end(key)
+            return self.cache_data[key]
+        return None
