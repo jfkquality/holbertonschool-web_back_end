7688b25df22294fc878f03627108fe3be5ebf2f3
jfkquality <jfkquality@gmail.com>
2020-10-27 04:45:40 +0000

    390. T.4. Bufix? Why didnt I pass test for key or item = None?

--- 0x03-caching/4-mru_cache.py
+++ 0x03-caching/4-mru_cache.py
@@ -14,20 +14,20 @@ class MRUCache(BaseCaching):
     def put(self, key, item):
         """ Setter method """
         self.cache_data[key] = item
-        self.cache_data.move_to_end(key)
+        self.cache_data.move_to_end(key, last=False)
         # print("CACHE {}".format(self.cache_data))
         if key is None or item is None:
             return
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             # last = list(self.cache_data.keys())[-1]
-            last = list(self.cache_data.keys())[0]
-            self.cache_data.pop(last)
-            # last = self.cache_data.popitem(last=False)
+            # last = list(self.cache_data.keys())[0]
+            # self.cache_data.pop(last)
+            last = self.cache_data.popitem(last=True)
             print("DISCARD: {}".format(last[0]))
 
     def get(self, key):
         """ Getter method """
         if key in self.cache_data and key is not None:
-            self.cache_data.move_to_end(key)
+            self.cache_data.move_to_end(key, last=False)
             return self.cache_data[key]
         return None
