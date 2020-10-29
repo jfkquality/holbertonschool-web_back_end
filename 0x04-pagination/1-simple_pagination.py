45f0f37ed85905ecee3c0cf14e06c2310a94f498
jfkquality <jfkquality@gmail.com>
2020-10-29 01:19:15 +0000

    585 t.1. Init commit.

---
+++ 0x04-pagination/1-simple_pagination.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+""" Pagination function
+"""
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return 2 member tuple of start, end index
+    """
+
+    return ((page - 1) * page_size, page * page_size)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """ Get a page """
+        assert (isinstance(page, int) and isinstance(page_size, int))
+        assert (page > 0 and page_size > 0)
+
+        range = index_range(page, page_size)
+        # print("RANGE ", range)
+        # print("RANGE 0 ", range[0])
+        # print("RANGE 1 ", range[1])
+
+        f = self.dataset()
+
+        return f[range[0]:range[1]]
