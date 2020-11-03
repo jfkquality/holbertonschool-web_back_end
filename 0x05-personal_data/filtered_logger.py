b0a34f052a2026d36a233789f5af88c38927176f
jfkquality <jfkquality@gmail.com>
2020-11-03 02:33:01 +0000

    602. t.1. Init commit.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ 0. Regex-ing """
 
 import re
+import logging
 from typing import List
 
 
@@ -12,3 +13,23 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(r'(' + f + '=).+?' + separator,
                          r'\1' + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format method """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
