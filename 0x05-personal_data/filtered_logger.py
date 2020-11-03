a970fddec512551d422eb61f652c0d62728ae869
jfkquality <jfkquality@gmail.com>
2020-11-03 05:01:41 +0000

    602. t.4. Init commit.

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -5,9 +5,29 @@ import re
 import logging
 import mysql.connector
 from typing import List
-from os import environ as e
+from os import environ as env
 
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Constructor """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ format method """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super().format(record)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -33,10 +53,10 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Get connector to db """
-    u = e['PERSONAL_DATA_DB_USERNAME']
-    p = e['PERSONAL_DATA_DB_PASSWORD']
-    h = e['PERSONAL_DATA_DB_HOST']
-    db = e['PERSONAL_DATA_DB_NAAME']
+    u = env['PERSONAL_DATA_DB_USERNAME']
+    p = env['PERSONAL_DATA_DB_PASSWORD']
+    h = env['PERSONAL_DATA_DB_HOST']
+    db = env['PERSONAL_DATA_DB_NAAME']
     # sql = mysql.connector.connect()
 
     return mysql.connector.connect(user=u,
@@ -45,21 +65,27 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                    database=db)
 
 
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
+def main():
+    """ main """
+    db = get.db()
+    cursor = db.cursor
+    cursor.execute("SELECT * FROM users;")
+    log = get_logger()
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
+    for i, row in enumerate(cursor):
+        msg = ""
+        for j, col in enumerate(cursor.description):
+            msg += "{}={}".format(col[0], row[j])
 
-    def __init__(self, fields: List[str]):
-        """ Constructor """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+    log.info(msg)
 
-    def format(self, record: logging.LogRecord) -> str:
-        """ format method """
-        record.msg = filter_datum(self.fields, self.REDACTION,
-                                  record.getMessage(), self.SEPARATOR)
-        return super().format(record)
+    formatter = RedactingFormatter(fields=list(PII_FIELDS))
+    for k - range(i + 1):
+        print(formatter.format(log_record))
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    """ driver code """
+    main()
