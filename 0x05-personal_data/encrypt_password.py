20500158bbd710a4e4295bc4d4e4cc498751e0d7
jfkquality <jfkquality@gmail.com>
2020-11-06 05:00:51 +0000

    602. t.6. Bugfix: Linter error.

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,6 +9,7 @@ def hash_password(password: str) -> bytes:
 
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Check valid password """
 
