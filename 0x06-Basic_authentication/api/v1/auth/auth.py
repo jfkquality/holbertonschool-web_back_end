37883e2cf00d17901428f67c8046081f4d9492c5
jfkquality <jfkquality@gmail.com>
2020-11-06 23:52:04 +0000

    380. t.4. Init commit.

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Creat Auth class """
 
+import os
 from typing import List, TypeVar
 from flask import request
 
@@ -9,7 +10,14 @@ class Auth:
     """ Auth class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require autho method """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        path = os.path.join(path, '')
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ auth header method """
