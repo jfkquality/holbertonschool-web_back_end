716b424b141035a9434a75461fce3a97393bb1d9
jfkquality <jfkquality@gmail.com>
2020-11-06 04:59:22 +0000

    602. t.6. Init commit.

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-""" Hash a password
-"""
+""" Hash a password """
 
 import bcrypt
 
@@ -9,3 +8,11 @@ def hash_password(password: str) -> bytes:
     """ Create salted hashed password """
 
     return bcrypt.hashpw(bytes(password, 'utf-8'), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Check valid password """
+
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    else:
+        return False
