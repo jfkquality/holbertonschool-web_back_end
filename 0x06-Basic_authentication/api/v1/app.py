2bad37bd4016f3e782f6c8459582e5f11adbed8c
jfkquality <jfkquality@gmail.com>
2020-11-07 02:59:17 +0000

    380. t.6. Init commit.

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -18,6 +18,10 @@ if os.environ.get('AUTH_TYPE') == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+if os.environ.get('AUTH_TYPE') == "basic_auth":
+    from api.v1.auth.auth import BasicAuth
+    auth = BasicAuth()
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env pyhhon3
+""" Basic Auth class """
+class BasicAuth(Auth):
+    """ Basic Autho class """
+    pass
