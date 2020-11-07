0eec284e8bbf50177d5d7f184bcfe09906617e74
jfkquality <jfkquality@gmail.com>
2020-11-07 03:48:37 +0000

    380. t.7. Init commit.

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python3
 """ Basic Auth class """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """ Basic Autho class """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """ extract header """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
