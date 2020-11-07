afb01ac30d7faa9dc11c735b94f591b855747124
jfkquality <jfkquality@gmail.com>
2020-11-07 02:17:32 +0000

    380. t.5. Init commit.

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -12,7 +12,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+if os.environ.get('AUTH_TYPE') == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -31,6 +35,19 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """ Forbidden hander """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_req():
+    """ Runs before any request. """
+    if auth is not None and not auth.require_auth(request.path,
+                         ['/api/v1/status/',
+                         '/api/v1/unauthorized/',
+                         '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401, description="unauthorized")
+        if auth.current_user(request) is None:
+            abort(403, description="forbidden")
 
 
 if __name__ == "__main__":
