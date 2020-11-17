475667dee1eea5688dd5198ba9af45dbde7d9552
jfkquality <jfkquality@gmail.com>
2020-11-17 20:59:59 +0000

    383. t.6. Init commit.

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -26,3 +26,8 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return SessionAuth.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        cookie = Auth.session_cookie(request)
+        user_id = self.user_id_for_session_id(cookie)
+        User.get(user_id)
