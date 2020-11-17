dbf285559e9e009db028cbf932e435929bb9db6a
jfkquality <jfkquality@gmail.com>
2020-11-17 06:50:53 +0000

    383. t.1. Bugfix: Forgot first line in session auth file: /usr/bin/env py3.

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -2,11 +2,11 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
+from os import getenv, environ
+# import os
 
 
 app = Flask(__name__)
@@ -14,15 +14,15 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-if os.environ.get('AUTH_TYPE') == "auth":
+if environ.get('AUTH_TYPE') == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
-if os.environ.get('AUTH_TYPE') == "basic_auth":
+if environ.get('AUTH_TYPE') == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 
-if os.environ.get('AUTH_TYPE') == "session_auth":
+if environ.get('AUTH_TYPE') == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+""" class SessionAuth. """
+
 from api.v1.auth.auth import Auth
+from models.user import User
+from typing import List, TypeVar
+import base64
 
 
 class SessionAuth(Auth):
+    """ class SessionAuth. """
     pass
