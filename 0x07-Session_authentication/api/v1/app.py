6e6310ff30faae852dfbfd06db9719166862f705
jfkquality <jfkquality@gmail.com>
2020-11-17 17:54:48 +0000

    383. t.5. Init commit.

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -52,8 +52,10 @@ def before_request():
     if auth is not None and auth.require_auth(request.path,
                                               ['/api/v1/status/',
                                                '/api/v1/unauthorized/',
-                                               '/api/v1/forbidden/']):
-        if auth.authorization_header(request) is None:
+                                               '/api/v1/forbidden/',
+                                               '/api/v1/auth_session/login/']):
+        if auth.authorization_header(request) is None and
+        auth.session_cookie(request) is None:
             abort(401, description="unauthorized")
         if auth.current_user(request) is None:
             abort(403, description="forbidden")
