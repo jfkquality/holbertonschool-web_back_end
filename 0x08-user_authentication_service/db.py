ca0822005186a36832a71622775223151eaf9ce8
jfkquality <jfkquality@gmail.com>
2020-11-18 04:34:50 +0000

    584. t.1. Refactor: Remove annotations. Test.

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -26,7 +26,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email, hashed_password):
         """ Add user """
         sess = self._session
         user = User(email=email, hashed_password=hashed_password)
