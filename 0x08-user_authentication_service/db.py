12d6bd77cb971d9122bef4aecad4c0e477626638
jfkquality <jfkquality@gmail.com>
2020-11-18 04:24:57 +0000

    584. t.1. Bugfix: Add doc strings.

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -9,12 +9,14 @@ from user import User
 class DB:
 
     def __init__(self):
+        """ Constructor """
         self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
+    """ Geter """
     def _session(self):
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
@@ -22,6 +24,7 @@ class DB:
         return self.__session
 
     def add_user(self, email, hashed_password):
+        """ Add user """
         sess = self._session
         user = User(email=email, hashed_password=hashed_password)
         sess.add(user)
