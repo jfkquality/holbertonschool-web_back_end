5c4b17ad3162ed298bbc207bcb705fecd7b1f3ec
jfkquality <jfkquality@gmail.com>
2020-11-19 08:03:29 +0000

    584. t.7. Bugfix: Linter errors.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -13,6 +13,7 @@ def hello():
     """ basic route """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def users() -> str:
     """ Add/Authenticate user """
@@ -22,6 +23,7 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
