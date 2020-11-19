9936e406b12a8ae7df07210174a942716cecad2e
jfkquality <jfkquality@gmail.com>
2020-11-19 08:01:34 +0000

    584. t.7. Init commit.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+
+Auth = Auth()
 
 app = Flask(__name__)
 
@@ -10,6 +13,15 @@ def hello():
     """ basic route """
     return jsonify({"message": "Bienvenue"})
 
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ Add/Authenticate user """
+    try:
+        user = Auth.register_user(request.form['email'],
+                                  request.form['password'])
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
