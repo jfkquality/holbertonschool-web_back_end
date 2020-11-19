f00dbb0641e92b04935574ae2ac01209baba22c7
jfkquality <jfkquality@gmail.com>
2020-11-19 06:49:35 +0000

    584. t.5. Init commit.

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,9 +1,25 @@
 #!/usr/bin/env python3
 
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
-# class Auth:
-#     """ Auth class """
+class Auth:
+    """ Auth class """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """ Add/register user if not exist """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            hpwd = _hash_password(password)
+            return self._db.add_user(email, hpwd)
 
 
 def _hash_password(password: str) -> str:
