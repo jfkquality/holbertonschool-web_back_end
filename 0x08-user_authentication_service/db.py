47ee09481b4a72aea65b607bc3930db0c5d3f491
jfkquality <jfkquality@gmail.com>
2020-11-19 02:07:16 +0000

    584. t.1. Change echo=True to False.

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -4,6 +4,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base
 from user import User
@@ -13,7 +15,7 @@ class DB:
 
     def __init__(self):
         """ Constructor """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -33,3 +35,17 @@ class DB:
         sess.add(usr)
         sess.commit()
         return usr
+
+    # def find_user_by(self, **kwargs):
+    #     sess = self._session
+    #     for k, v in kwargs.items():
+    #         # print("KEY:", k, "VALUE:", v)
+    #         key = getattr(User, k)
+    #         # print("SESSION QUERY KEY:", sess.query(key).
+    # filter_by(email=str(v)))
+    #         if not sess.query(key).filter(key==v):
+    #             raise NoResultFound()
+    #         if sess.query(key).filter(key==v):
+    #             return sess.query(key).filter(key==v)
+    #     # return sess.query(User, key)
+    #     # raise InvalidRequestError()
