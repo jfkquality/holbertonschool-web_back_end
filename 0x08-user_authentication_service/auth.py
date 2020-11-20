43529fa39ff7bbefbbfbf72f1a6d8c5e1d587242
jfkquality <jfkquality@gmail.com>
2020-11-20 06:25:56 +0000

    584. t.8. Init commit.

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -14,13 +14,24 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """ Add/register user if not exist """
+        """ 7. Register/add user if not exist """
         try:
             user = self._db.find_user_by(email=email)
             raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             hpwd = _hash_password(password)
             return self._db.add_user(email, hpwd)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ 8. Credentials validation """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(), user.hashed_password):
+                return True
+            else:
+                raise Exception()
+        except Exception:
+            return False
 
 
 def _hash_password(password: str) -> str:
