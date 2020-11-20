dcca713155110682f1bbb102f9cdb971fd46c431
jfkquality <jfkquality@gmail.com>
2020-11-20 21:53:06 +0000

    584. t.11. Undo refactor. Combine form and resp variables into make_response.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -29,28 +29,27 @@ def users() -> str:
 def login():
     """ Login function """
     # d = request.form
-    # email = request.form['email']
-    # password = request.form['password']
-    # if AUTH.valid_login(email, password):
-    #     sess = AUTH.create_session(email)
-    #     form = {"email": email, "message": "logged in"}
-    #     resp = make_response(form)
-    #     resp.set_cookie('session_id', sess)
-    #     return resp
-    # else:
-    #     abort(401)
-
-    data = request.form
-
-    valid = AUTH.valid_login(data['email'], data['password'])
-    if valid:
-        sess = AUTH.create_session(data['email'])
-        resp = make_response({"email": data['email'],
-                              "message": "logged in"})
-        resp.set_cookie("session_id", sess)
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        sess = AUTH.create_session(email)
+        resp = make_response({"email": email, "message": "logged in"})
+        # resp = make_response(form)
+        resp.set_cookie('session_id', sess)
         return resp
     else:
         abort(401)
+
+    # data = request.form
+
+    # if AUTH.valid_login(data['email'], data['password']):
+    #     sess = AUTH.create_session(data['email'])
+    #     resp = make_response({"email": data['email'],
+    #                           "message": "logged in"})
+    #     resp.set_cookie("session_id", sess)
+    #     return jsonify(resp)
+    # else:
+    #     abort(401)
 
 
 if __name__ == "__main__":
