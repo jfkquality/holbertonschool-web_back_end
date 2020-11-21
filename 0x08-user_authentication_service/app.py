2b0b33f6a708eeef55a2b7f481fb8f505e059b93
jfkquality <jfkquality@gmail.com>
2020-11-21 03:22:06 +0000

    584. T.15. Init commit.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -62,6 +62,16 @@ def logout():
             return redirect('/')
     abort(403)
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile:
+    """ 15. User profile """
+    sess = request.cookies.get('session_id')
+    if sess:
+        user = AUTH.get_user_from_session_id(sess)
+        if user:
+            return jsonify({"email": "<user email>"}), 200
+    abort(403)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
