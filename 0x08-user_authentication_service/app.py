9973cb4983ffd80885fcf6aa8587098ef6940173
jfkquality <jfkquality@gmail.com>
2020-11-21 06:23:38 +0000

    584. t.17. init commit.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -73,6 +73,17 @@ def profile():
             return jsonify({"email": user.email}), 200
     abort(403)
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def get_reset_password_token():
+    """ 17. Get reset password token """
+    email = request.form['email']
+    try:
+        token = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+
+    return jsonify({"email": email, "reset_token": token}), 200
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
