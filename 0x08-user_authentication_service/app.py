84d0f5797cdbe5076665ed5ba6c7633c95f99b64
jfkquality <jfkquality@gmail.com>
2020-11-21 02:49:01 +0000

    584. T.14. Bugfix: Just return 403.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -60,7 +60,7 @@ def logout():
         AUTH.destroy_session(user.id)
         return redirect('/')
     else:
-        return render_template(), 403
+        return 403
 
 
 if __name__ == "__main__":
