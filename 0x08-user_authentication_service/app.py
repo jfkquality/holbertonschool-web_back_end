16cdd5b02a7f8a374b1ce365792dd707160c36f2
jfkquality <jfkquality@gmail.com>
2020-11-21 07:59:56 +0000

    584. bugfix. Linter error.

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -85,6 +85,7 @@ def get_reset_password_token():
 
     return jsonify({"email": email, "reset_token": token}), 200
 
+
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password():
     """ 19. Update password end-point """
