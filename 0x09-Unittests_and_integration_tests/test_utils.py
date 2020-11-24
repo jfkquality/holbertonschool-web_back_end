fcf641709949a9c1a11a39ff55a0d4cfbf7a85f4
jfkquality <jfkquality@gmail.com>
2020-11-24 00:47:35 +0000

    610. t.1. init commit.

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -12,19 +12,28 @@ from typing import (
     Callable,
 )
 
-# import unittest
+anm = access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ 0. Parameterize a unit test """
+    """ test access nested map utils function """
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expect):
-        """ test access nested map utils function """
-        self.assertEqual(access_nested_map(nested_map, path), expect)
+        """ 0. Parameterize a unit test """
+        self.assertEqual(anm(nested_map, path), expect)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """" 1. Parameterize a unit test """
+        with self.assertRaises(KeyError):
+            anm(nested_map, path)
 
 
 if __name__ == '__main__':
