0efea54e3ca64f25b08b2e64928ef5eab8baf152
jfkquality <jfkquality@gmail.com>
2020-11-30 02:57:05 +0000

    631. t.0. bugfix: Add doc strings.

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
-
+""" 0. Basic Flask app """
 from flask import Flask, render_template
-from flask_babel import Babel
+# from flask_babel import Babel
 
 app = Flask(__name__)
 # from app import routes
 
-babel = Babel(app)
+# babel = Babel(app)
 
 
 @app.route('/')
 def index():
+    """ Main page """
     title = "Welcome to Holberton"
-    header = "Hello World!"
-    return render_template(templates/0-index.html, title=title, header=header)
+    header = "Hello World"
+    return render_template('0-index.html', title=title, header=header)
 
 
 if __name__ == "__main__":
