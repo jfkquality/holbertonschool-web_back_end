33a48aca92d44a749d26c1800f7ea9fdd7555344
jfkquality <jfkquality@gmail.com>
2020-11-30 04:49:47 +0000

    631. t.4. init commit.

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" 0. Basic Flask app """
+from flask import Flask, render_template, g
+from flask_babel import Babel
+
+app = Flask(__name__)
+# from app import routes
+
+babel = Babel(app)
+
+
+class Config():
+    """ Babel  Config class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+config = Config()
+app.config.from_object(config)
+
+
+@app.route('/')
+def index():
+    """ Main page """
+    title = "Welcome to Holberton"
+    header = "Hello World"
+    return render_template('4-index.html', title=title, header=header)
+
+
+@babel.localeselector
+def get_locale():
+    """ Get lcoale """
+    return request.accept_languages.best_match(config.LANGUAGES)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
