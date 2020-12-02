6595e8ec17146bb9f4b1be93a5e0b007dbe8c287
jfkquality <jfkquality@gmail.com>
2020-12-02 03:27:10 +0000

    630. t.0. bugfix. FORGOT SELF IN STORE() DEF.

---
+++ 0x0B_redis_basic/0-main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+from exercise import Cache
+
+cache = Cache()
+
+print(cache.store("Goodbye"))
--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,13 +7,13 @@ from typing import Union
 
 
 class Cache():
-    """ Cache class """
+    """ Cache class for Redis """
     def __init__(self):
         """ 0. Writing strings to Redis """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(data: Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """ Create key; store data in redis """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
