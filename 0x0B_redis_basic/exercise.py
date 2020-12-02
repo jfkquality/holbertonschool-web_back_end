33f6a366ec43ee6e99a1caff9e352e8e18ba7c42
jfkquality <jfkquality@gmail.com>
2020-12-02 02:30:40 +0000

    630. t.0. init commit.

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""630. 0x0B. Redis basic """
+
+import redis
+import uuid
+from typing import Any
+
+
+class Cache():
+    """ Cache class """
+    def __init__(self):
+        """ 0. Writing strings to Redis """
+        self._redis = redis.Redis
+        self._redis.flushdb()
+
+    def store(data: Any[str, bytes, int, float]) -> str:
+        """ Create key; store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set({key, data})
+        return key
