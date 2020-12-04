cc935c09fc93b9735fa21c30b897ffe543c0eaca
jfkquality <jfkquality@gmail.com>
2020-12-04 07:15:15 +0000

    632. t.0. bugfix: Typo VarCher for name.

--- 0x0C-MySQL_Advanced/0-uniq_users.sql
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -5,5 +5,5 @@
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        email VARCHAR(255) NOT NULL UNIQUE,
-       name VARCHER(255)
+       name VARCHAR(255)
        )
