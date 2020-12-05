c0fe93b5091b172daf0e7fc86d31b9776a0228aa
jfkquality <jfkquality@gmail.com>
2020-12-05 03:07:28 +0000

    632. t.3. init commit.

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,4 @@
+-- 2. Best band ever!
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
 -- Column names must be: origin and nb_fans
 SELECT origin, SUM(fans) AS nb_fans
