780afd001c89757a68cd4025204d8592d897c821
jfkquality <jfkquality@gmail.com>
2020-12-05 01:45:36 +0000

    632. t.2. bugfix: Comment type L2. Add Group by

--- 0x0C-MySQL_Advanced/2-fans.sql
+++ 0x0C-MySQL_Advanced/2-fans.sql
@@ -1,9 +1,6 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
---Column names must be: origin and nb_fans
-SELECT
-	origin
-	fans AS nb_fans
-FROM
-	metal_bands
-ORDER BY
-      nb_fans DESC
+-- Column names must be: origin and nb_fans
+SELECT origin, SUM(fans) AS nb_fans
+FROM metal_bands
+GROUP BY origin
+ORDER BY nb_fans DESC;
