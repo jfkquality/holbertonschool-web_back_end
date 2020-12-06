d74cb537a744083019a920e61fc25cf35481740d
jfkquality <jfkquality@gmail.com>
2020-12-06 04:56:41 +0000

    632. t.6, init commit. Incomplete. Push to save from container.

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,9 @@
+-- 6. Add bonus
+--  creates a stored procedure AddBonus that adds a new correction for a student.
+CREATE PROCEDURE AddBonus(IN user_id INT, project_name VARCHAR(255), score INT)
+BEGIN
+DECLARE variables
+DECLARE CURSORS
+DECLARE handlers
+STATEMENTS
+END
