8af51e3b6a016bd03570fe7e2febe038afb6c68e
jfkquality <jfkquality@gmail.com>
2020-12-07 15:34:34 +0000

    630. t.5. bugfix: Add 'ur' as parameter.

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -8,6 +8,7 @@ Cache = __import__('exercise').Cache
 cache = Cache()
 
 
-def get_page():
+def get_page(url):
     """ track how many times a particular URL was accessed """
-    pass
+    response = requests.get(url)
+    return response.json  # response.text
