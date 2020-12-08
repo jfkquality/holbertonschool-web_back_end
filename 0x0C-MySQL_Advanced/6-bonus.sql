2891b7cd2b230c65c70f43f1669c6adc1881df75
jfkquality <jfkquality@gmail.com>
2020-12-08 06:38:49 +0000

    632. t.6. init commit.

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,9 +1,22 @@
 -- 6. Add bonus
 --  creates a stored procedure AddBonus that adds a new correction for a student.
-CREATE PROCEDURE AddBonus(IN user_id INT, project_name VARCHAR(255), score INT)
+DELIMITER $$
+
+CREATE PROCEDURE AddBonus (IN user_id INT, project_name VARCHAR(255), score INT)
 BEGIN
-DECLARE variables
-DECLARE CURSORS
-DECLARE handlers
-STATEMENTS
-END
+	IF NOT EXISTS (
+	   SELECT *
+	   FROM projects
+	   WHERE name = project_name)
+	   THEN
+	   	INSERT INTO projects(name) VALUES(project_name);
+	END IF;
+
+	INSERT INTO corrections(user_id, score,  project_id)
+	       VALUES(user_id, score,
+			(SELECT id from projects
+			WHERE name = project_name)
+			);
+END$$
+
+DELIMITER ;
