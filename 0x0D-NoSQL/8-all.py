b6e8126cef2baf8c8a4330682df967a08698eed1
jfkquality <jfkquality@gmail.com>
2020-12-08 19:57:12 -0800

    411. t.8. bugfix: JUST RETURN COLLECTION.FIND()ga ..

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python3
 """ 8. List all documents in Python """
 
-from pymongo import MongoClient
-
 
 def list_all(mongo_collection):
     """ list all docs in a collection """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client["mongo_collection"]
-    return db.mongo_collection.find()
+    return mongo_collection.find()
