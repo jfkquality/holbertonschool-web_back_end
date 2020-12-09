34885362f743272572d5afbea95857f2293c0154
jfkquality <jfkquality@gmail.com>
2020-12-08 21:15:56 -0800

    411. t.14/101. bugfix: Linterr errors.

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env pytho3
 """ 14. Top students """
 
+
 def top_students(mongo_collection):
     """ returns all students sorted by average score """
     return mongo_collection.aggregate({
-        "$group": {"avg_score": {avg: core}
-        }
+        "$group": {"avg_score": {avg: core}}
     })
