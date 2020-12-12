5853bd7d5c2ffa5281a102a310497037151a3a9a
jfkquality <jfkquality@gmail.com>
2020-12-12 16:51:56 +0000

    577. t.8. Refactor: Linter errors.

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -2,7 +2,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   // let idx = 0;
   for (const value of array) {
-    idx = array.indexOf(value);  // from old code. Much better than counter.
+    const idx = array.indexOf(value); // from old code. Much better than counter.
     array[idx] = appendString + value;
     // idx += 1;
   }
