d8718fab5d682556414cf4dc7d0941f155ab8c25
jfkquality <jfkquality@gmail.com>
2020-12-15 07:17:13 +0000

    577. t-14/101. bugfix: use join().

--- 0x0E-ES6_basic/101-iterateThroughObject.js
+++ 0x0E-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,11 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let names;
-  for (const item of reportWithIterator) {
-    names.concat(item);
-    if (!item.done) {
-      names.concat(' | ');
-    }
-  }
+  reportWithIterator.join(' | ');
+  // let names;
+  // for (const item of reportWithIterator) {
+  //   names.concat(item);
+  //   if (!item.done) {
+  //     names.concat(' | ');
+  //   }
+  // }
   return names;
 }
