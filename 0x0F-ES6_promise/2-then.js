1e74c2e2d2c2ace45f3c828e05a278926006d201
jfkquality <jfkquality@gmail.com>
2020-12-17 07:17:45 +0000

    578. t.2. bugfix: linter errors.

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,12 +1,15 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => (
-    {
-      status: 200,
-      body: 'Success',
-    }), () => {
-      Error();
-    });
   promise.finally(() => {
     console.log('Got a response from the API');
   });
+  promise.then(
+    () => (
+      {
+        status: 200,
+        body: 'Success',
+      }),
+    () => {
+      Error();
+    },
+  );
 }
