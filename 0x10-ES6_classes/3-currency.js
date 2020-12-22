255ebdbd8e488bb41f517283a379170c759f8c66
jfkquality <jfkquality@gmail.com>
2020-12-22 04:38:08 +0000

    579. t.3. bufix: MULTIPLE TYPOS IN WORD CONSTRUCTOR!

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,5 +1,5 @@
 export default class Currency {
-  contstructore(code, name) {
+  constructor(code, name) {
     this._code = code;
     this._name = name;
   }
@@ -21,6 +21,6 @@ export default class Currency {
   }
 
   displayFullCurrency() {
-    return this._name + '(' + this._code + ')';
+    return `${this._name} (${this._code})`;
   }
 }
