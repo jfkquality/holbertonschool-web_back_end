1153e1dbc090bbec35b5d96094d539a28ce02dbf
jfkquality <jfkquality@gmail.com>
2020-12-22 00:07:37 +0000

    579. t.1. bugfix: create array then return it.

--- 0x10-ES6_classes/1-make_classrooms.js
+++ 0x10-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,24 @@
 import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  return [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
+export default function initializeRooms() {
+  const rooms = [];
+  const room1 = new ClassRoom(19);
+  rooms.push(room1);
+  const room2 = new ClassRoom(20);
+  rooms.push(room2);
+  const room3 = new ClassRoom(34);
+  rooms.push(room3);
+  return rooms;
+  // [
+  //   new ClassRoom(19),
+  //   new ClassRoom(20),
+  //   new ClassRoom(34)
+  // ];
+  // [
+  //   Object(new ClassRoom(19)),
+  //   Object(new ClassRoom(20)),
+  //   Object(new ClassRoom(34))
+  // ];
 }
 
-initializeRooms();
+// initializeRooms();
