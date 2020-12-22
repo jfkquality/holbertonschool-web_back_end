24b27b74ec1d0014e4c948bacf75c693942a8dc8
jfkquality <jfkquality@gmail.com>
2020-12-22 04:06:37 +0000

    579. t.2. bugfix: add type checks to setters!

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -29,14 +29,25 @@ export default class HolbertonCourse {
   }
 
   set name(name) {
+    if (typeof name !== 'string') {
+      throw TypeError('Name must be a string');
+    }
     this._name = name;
   }
 
   set length(length) {
+    if (typeof (length) !== 'number') {
+      throw TypeError('Length must be a number');
+    }
     this._length = length;
   }
 
   set students(students) {
-    this._students = students;
+    if (Array.isArray(students)
+        && students.every((student) => typeof (student) === 'string')) {
+      this._students = students;
+    } else {
+      throw TypeError('students must be an Array of strings');
+    }
   }
 }
