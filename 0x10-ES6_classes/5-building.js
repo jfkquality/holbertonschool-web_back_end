e030bdfdc013e71f33d05db3034e030344746a6b
jfkquality <jfkquality@gmail.com>
2020-12-24 04:43:23 +0000

    579. refactor: put back my code with correct 2nd test for method override.

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -1,12 +1,30 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    // console.log("THIS =", this);
+    // console.log("THIS CONSTRUCTOR =", this.constructor);
+    // console.log("THIS CONSTRUCTOR prototype =", this.constructor.prototype);
+    // console.log("INSTANCE OF THIS =", this instanceof Building);
+    // console.log("Building.prototype.evacuationWarningMessage =",
+    // Building.prototype.evacuationWarningMessage);
+    // console.log("this.evacuationWarningMessage =", this.evacuationWarningMessage);
+    // console.log("Super evacuationWarningMessage =", super.evacuationWarningMessage);
+    // console.log("SUPER CONSTRUCTOR =", super.prototype);
+    // console.log("this.constructor === Building", this.constructor === Building);
+    if (this.constructor !== Building
+    // && this.evacuationWarningMessage) === undefined) { //super.evacuationWarningMessage
+       && this.evacuationWarningMessage !== Building.prototype.evacuationWarningMessage
+    ) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
   }
+
+  // evacuationWarningMessage() {
+  //   console.log('HELLO');
+  // }
 }
+// Building.evacuationWarningMessage();
