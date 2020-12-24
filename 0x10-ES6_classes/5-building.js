d81bc5f2729a019781acded38531031d9c29a26c
jfkquality <jfkquality@gmail.com>
2020-12-24 04:37:22 +0000

    579. bugfix: linter errors.

--- 0x10-ES6_classes/5-building.js
+++ 0x10-ES6_classes/5-building.js
@@ -4,15 +4,16 @@ export default class Building {
     // console.log("THIS CONSTRUCTOR =", this.constructor);
     // console.log("THIS CONSTRUCTOR prototype =", this.constructor.prototype);
     // console.log("INSTANCE OF THIS =", this instanceof Building);
-    // console.log("Building.prototype.evacuationWarningMessage =", Building.prototype.evacuationWarningMessage);
+    // console.log("Building.prototype.evacuationWarningMessage =",
+    // Building.prototype.evacuationWarningMessage);
     // console.log("this.evacuationWarningMessage =", this.evacuationWarningMessage);
     // console.log("Super evacuationWarningMessage =", super.evacuationWarningMessage);
     // console.log("SUPER CONSTRUCTOR =", super.prototype);
     // console.log("this.constructor === Building", this.constructor === Building);
-    if (this.constructor !== Building //) { // 'Building {}'
-	// && this.evacuationWarningMessage) === undefined) { //super.evacuationWarningMessage
-	// && this.evacuationWarningMessage == Building.prototype.evacuationWarningMessage
-       ) {
+    if (this.constructor !== Building
+    // && this.evacuationWarningMessage) === undefined) { //super.evacuationWarningMessage
+    // && this.evacuationWarningMessage == Building.prototype.evacuationWarningMessage
+    ) {
       throw Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
