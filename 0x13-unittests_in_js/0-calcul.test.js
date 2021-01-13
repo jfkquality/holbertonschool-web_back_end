cf687f216b73887150a1f659224712a69b5810c6
jfkquality <jfkquality@gmail.com>
2021-01-13 22:06:08 +0000

    614. t.0. bugfix: reverse it() arguments.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -2,9 +2,9 @@ const assert = require('assert');
 const calculateNumber = require("./0-calcul.js");
 
 describe('Calculate', () => {
-  describe("round1", () => {
+  // describe("round1", () => {
     it('should return 5', () => {
-      assert.equal(calculateNumber(1.4, 3.7), 5);
+      assert.equal(5, calculateNumber(1.4, 3.7));
     });
-  });
+  // });
 });
