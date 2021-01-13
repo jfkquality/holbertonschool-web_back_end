6426fa8a7e82b126c08cb2664cd8c909aac17f01
jfkquality <jfkquality@gmail.com>
2021-01-13 23:57:28 +0000

    614. t.0. refactor: reverse assert arguments again.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -4,16 +4,16 @@ const calculateNumber = require("./0-calcul.js");
 describe('Calculate', () => {
   // describe("round1", () => {
     it('should equal 5', () => {
-      assert.equal(5, calculateNumber(1.4, 3.7));
+      assert.equal(calculateNumber(1.4, 3.7), 5);
     });
     it('should equal 5', () => {
-      assert.equal(5, calculateNumber(1, 3.7));
+      assert.equal(calculateNumber(1, 3.7), 5);
     });
     it('should equal 4', () => {
-      assert.equal(4, calculateNumber(1.4, 3.2));
+      assert.equal(calculateNumber(1.4, 3.2), 4);
     });
     it('should equal 7', () => {
-      assert.equal(6, calculateNumber(1.5, 3.7));
+      assert.equal(calculateNumber(1.5, 3.7), 6);
     });
   // });
 });
