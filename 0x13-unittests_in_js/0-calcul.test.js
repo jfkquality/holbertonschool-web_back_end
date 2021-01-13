85d2d2372fddcee10ad4744741864b8938e69f93
jfkquality <jfkquality@gmail.com>
2021-01-13 22:10:34 +0000

    614. t.0. bugfix? add more tests.

--- 0x13-unittests_in_js/0-calcul.test.js
+++ 0x13-unittests_in_js/0-calcul.test.js
@@ -3,8 +3,17 @@ const calculateNumber = require("./0-calcul.js");
 
 describe('Calculate', () => {
   // describe("round1", () => {
-    it('should return 5', () => {
+    it('should equal 5', () => {
       assert.equal(5, calculateNumber(1.4, 3.7));
+    });
+    it('should equal 5', () => {
+      assert.equal(5, calculateNumber(1, 3.7));
+    });
+    it('should equal 4', () => {
+      assert.equal(4, calculateNumber(1.4, 3.2));
+    });
+    it('should equal 7', () => {
+      assert.equal(6, calculateNumber(1.5, 3.7));
     });
   // });
 });
