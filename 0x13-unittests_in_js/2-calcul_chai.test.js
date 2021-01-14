bc1375c8bd3ddb67eb0d21f49fffb41fb0eeaa25
jfkquality <jfkquality@gmail.com>
2021-01-14 00:01:22 +0000

    614. t.1. refactor: reverse assert arguments again.

---
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,67 @@
+const assert = require('assert');
+const calculateNumber = require("./1-calcul.js");
+
+describe('Calculate', () => {
+  describe("sum", () => {
+    it('should equal 5', () => {
+      assert.equal(5, calculateNumber(1.4, 3.7, 'SUM'), 5);
+    });
+    it('should equal 5', () => {
+      assert.equal(5, calculateNumber(1, 3.7, 'SUM'), 5);
+    });
+    it('should equal 6', () => {
+      assert.equal(6, calculateNumber(1.5, 3.5, 'SUM'), 6);
+    });
+    it('should equal 5', () => {
+      assert.equal(5, calculateNumber(1.5, 3.2, 'SUM'), 5);
+    });
+  });
+
+  describe("subtract", () => {
+    it('should equal 5', () => {
+      assert.equal(-2, calculateNumber(1.5, 3.7, 'SUBTRACT'), -2);
+    });
+    it('should equal -3', () => {
+      assert.equal(-3, calculateNumber(1, 3.7, 'SUBTRACT'), -3);
+    });
+    it('should equal 1', () => {
+      assert.equal(1, calculateNumber(5.4, 3.5, 'SUBTRACT'), 1);
+    });
+    it('should equal 0', () => {
+      assert.equal(0, calculateNumber(5.4, 5, 'SUBTRACT'), 0);
+    });
+    it('should equal 6', () => {
+      assert.equal(6, calculateNumber(5.4, -.6, 'SUBTRACT'), 6);
+    });
+    it('should equal 5', () => {
+      assert.equal(5, calculateNumber(5.4, .4, 'SUBTRACT'), 5);
+    });
+    it('should equal 0', () => {
+      assert.equal(0, calculateNumber(5.4, 5, 'SUBTRACT'), 0);
+    });
+  });
+
+  describe("divide", () => {
+    it('should equal .25', () => {
+      assert.equal(.25, calculateNumber(1.4, 3.7, 'DIVIDE'), .25);
+    });
+    it('should equal 1', () => {
+      assert.equal(1, calculateNumber(4, 3.7, 'DIVIDE'), 1);
+    });
+    it('should equal 0', () => {
+      assert.equal(0, calculateNumber(0, 3.5, 'DIVIDE'), 0);
+    });
+    it('should equal Error', () => {
+      assert.equal('Error', calculateNumber(1.5, .4, 'DIVIDE'), 'Error');
+    });
+    it('should equal Error', () => {
+      assert.equal('Error', calculateNumber(1.5, -.4, 'DIVIDE'), 'Error');
+    });
+    it('should equal -2', () => {
+      assert.equal(-2, calculateNumber(1.5, -.6, 'DIVIDE'), -2);
+    });
+    it('should equal Error', () => {
+      assert.equal('Error', calculateNumber(1.5, .4, 'DIVIDE'), 'Error');
+    });
+  });
+});
