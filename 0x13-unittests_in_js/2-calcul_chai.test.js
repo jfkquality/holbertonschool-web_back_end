4e88690a85bde5444409d5974d088e2016691ea3
jfkquality <jfkquality@gmail.com>
2021-01-14 00:27:25 +0000

    614. t.2. Init commit.

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,67 +1,68 @@
-const assert = require('assert');
+const chai = require("chai");
+const expect = chai.expect;
 const calculateNumber = require("./1-calcul.js");
 
 describe('Calculate', () => {
   describe("sum", () => {
     it('should equal 5', () => {
-      assert.equal(5, calculateNumber(1.4, 3.7, 'SUM'), 5);
+      expect(calculateNumber(1.4, 3.7, 'SUM')).to.equal(5);
     });
     it('should equal 5', () => {
-      assert.equal(5, calculateNumber(1, 3.7, 'SUM'), 5);
+      expect(5, calculateNumber(1, 3.7, 'SUM'));
     });
     it('should equal 6', () => {
-      assert.equal(6, calculateNumber(1.5, 3.5, 'SUM'), 6);
+      expect(6, calculateNumber(1.5, 3.5, 'SUM'));
     });
     it('should equal 5', () => {
-      assert.equal(5, calculateNumber(1.5, 3.2, 'SUM'), 5);
+      expect(5, calculateNumber(1.5, 3.2, 'SUM'));
     });
   });
 
   describe("subtract", () => {
     it('should equal 5', () => {
-      assert.equal(-2, calculateNumber(1.5, 3.7, 'SUBTRACT'), -2);
-    });
-    it('should equal -3', () => {
-      assert.equal(-3, calculateNumber(1, 3.7, 'SUBTRACT'), -3);
-    });
-    it('should equal 1', () => {
-      assert.equal(1, calculateNumber(5.4, 3.5, 'SUBTRACT'), 1);
-    });
-    it('should equal 0', () => {
-      assert.equal(0, calculateNumber(5.4, 5, 'SUBTRACT'), 0);
-    });
-    it('should equal 6', () => {
-      assert.equal(6, calculateNumber(5.4, -.6, 'SUBTRACT'), 6);
+      expect(-2, calculateNumber(1.5, 3.7, 'SUBTRACT'));
     });
     it('should equal 5', () => {
-      assert.equal(5, calculateNumber(5.4, .4, 'SUBTRACT'), 5);
+      expect(-3, calculateNumber(1, 3.7, 'SUBTRACT'));
+    });
+    it('should equal 1', () => {
+      expect(1, calculateNumber(5.4, 3.5, 'SUBTRACT'));
+    });
+    it('should equal 7', () => {
+      expect(0, calculateNumber(5.4, 5, 'SUBTRACT'));
+    });
+    it('should equal 6', () => {
+      expect(6, calculateNumber(5.4, -.6, 'SUBTRACT'));
+    });
+    it('should equal 5', () => {
+      expect(5, calculateNumber(5.4, .4, 'SUBTRACT'));
     });
     it('should equal 0', () => {
-      assert.equal(0, calculateNumber(5.4, 5, 'SUBTRACT'), 0);
+      expect(0, calculateNumber(5.4, 5, 'SUBTRACT'));
     });
   });
 
   describe("divide", () => {
     it('should equal .25', () => {
-      assert.equal(.25, calculateNumber(1.4, 3.7, 'DIVIDE'), .25);
+      expect(.25, calculateNumber(1.4, 3.7, 'DIVIDE'));
     });
     it('should equal 1', () => {
-      assert.equal(1, calculateNumber(4, 3.7, 'DIVIDE'), 1);
+      expect(1, calculateNumber(4, 3.7, 'DIVIDE'));
     });
     it('should equal 0', () => {
-      assert.equal(0, calculateNumber(0, 3.5, 'DIVIDE'), 0);
+      expect(0, calculateNumber(0, 3.5, 'DIVIDE'));
     });
     it('should equal Error', () => {
-      assert.equal('Error', calculateNumber(1.5, .4, 'DIVIDE'), 'Error');
+      expect('Error', calculateNumber(1.5, .4, 'DIVIDE'));
     });
     it('should equal Error', () => {
-      assert.equal('Error', calculateNumber(1.5, -.4, 'DIVIDE'), 'Error');
+      expect('Error', calculateNumber(1.5, -.4, 'DIVIDE'));
     });
     it('should equal -2', () => {
-      assert.equal(-2, calculateNumber(1.5, -.6, 'DIVIDE'), -2);
+      expect(-2, calculateNumber(1.5, -.6, 'DIVIDE'));
     });
     it('should equal Error', () => {
-      assert.equal('Error', calculateNumber(1.5, .4, 'DIVIDE'), 'Error');
+      expect('Error', calculateNumber(1.5, .4, 'DIVIDE'));
     });
   });
 });
