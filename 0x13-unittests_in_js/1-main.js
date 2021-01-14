84b1b4315a4060ec34afd849eec5aab6d0d0cb02
jfkquality <jfkquality@gmail.com>
2021-01-14 04:13:03 +0000

    614. t.2. Bugfix: Fix function parameters order._

--- 0x13-unittests_in_js/1-main.js
+++ 0x13-unittests_in_js/1-main.js
@@ -1,10 +1,10 @@
 const calcul = require("./1-calcul.js");
-console.log("1 + 3 = ", calcul(1, 3, 'SUM'));
+console.log("1 + 3 = ", calcul('SUM', 1, 3));
 
-console.log("1 + 3.7 = ", calcul(1, 3.7, 'SUBTRACT'));
+console.log("1 + 3.7 = ", calcul('SUBTRACT', 1, 3.7));
 
-console.log("1.2 + 3.7 = ", calcul(1.2, 3.7, 'DIVIDE'));
+console.log("1.2 + 3.7 = ", calcul('DIVIDE', 1.2, 3.7));
 
-console.log("1.5 + 3.7 = ", calcul(1.4, 3.2, 'DIVIDE'));
+console.log("1.5 + 3.7 = ", calcul('DIVIDE', 1.4, 3.2));
 
-console.log("1.5 + 3.7 = ", calcul(1.5, .4, 'DIVIDE'));
+console.log("1.5 + 3.7 = ", calcul('DIVIDE', 1.5, .4));
--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -5,64 +5,64 @@ const calculateNumber = require("./2-calcul_chai.js");
 describe('Calculate', () => {
   describe("sum", () => {
     it('should equal 5', () => {
-      expect(calculateNumber(1.4, 3.7, 'SUM')).to.equal(5);
+      expect(calculateNumber('SUM', 1.4, 3.7)).to.equal(5);
     });
     it('should equal 5', () => {
-      expect(calculateNumber(1, 3.7, 'SUM')).to.equal(5);
+      expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
     });
     it('should equal 6', () => {
-      expect(calculateNumber(1.5, 3.5, 'SUM')).to.equal(6);
+      expect(calculateNumber('SUM', 1.5, 3.5)).to.equal(6);
     });
     it('should equal 5', () => {
-      expect(calculateNumber(1.5, 3.2, 'SUM')).to.equal(5);
+      expect(calculateNumber('SUM', 1.5, 3.2)).to.equal(5);
     });
   });
 
   describe("subtract", () => {
     it('should equal -2', () => {
-      expect(calculateNumber(1.5, 3.7, 'SUBTRACT')).to.equal(-2);
+      expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
     });
     it('should equal -3', () => {
-      expect(calculateNumber(1, 3.7, 'SUBTRACT')).to.equal(-3);
+      expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
     });
     it('should equal 1', () => {
-      expect(calculateNumber(5.4, 3.5, 'SUBTRACT')).to.equal(1);
+      expect(calculateNumber('SUBTRACT', 5.4, 3.5)).to.equal(1);
     });
     it('should equal 0', () => {
-      expect(calculateNumber(5.4, 5, 'SUBTRACT')).to.equal(0);
+      expect(calculateNumber('SUBTRACT', 5.4, 5)).to.equal(0);
     });
     it('should equal 6', () => {
-      expect(calculateNumber(5.4, -.6, 'SUBTRACT')).to.equal(6);
+      expect(calculateNumber('SUBTRACT', 5.4, -.6)).to.equal(6);
     });
     it('should equal 5', () => {
-      expect(calculateNumber(5.4, .4, 'SUBTRACT')).to.equal(5);
+      expect(calculateNumber('SUBTRACT', 5.4, .4)).to.equal(5);
     });
     it('should equal 0', () => {
-      expect(calculateNumber(5.4, 5, 'SUBTRACT')).to.equal(0);
+      expect(calculateNumber('SUBTRACT', 5.4, 5)).to.equal(0);
     });
   });
 
   describe("divide", () => {
     it('should equal .25', () => {
-      expect(calculateNumber(1.4, 3.7, 'DIVIDE')).to.equal(.25);
+      expect(calculateNumber('DIVIDE', 1.4, 3.7)).to.equal(.25);
     });
     it('should equal 1', () => {
-      expect(calculateNumber(4, 3.7, 'DIVIDE')).to.equal(1);
+      expect(calculateNumber('DIVIDE', 4, 3.7)).to.equal(1);
     });
     it('should equal 0', () => {
-      expect(calculateNumber(0, 3.5, 'DIVIDE')).to.equal(0);
+      expect(calculateNumber('DIVIDE', 0, 3.5)).to.equal(0);
     });
     it('should equal Error', () => {
-      expect(calculateNumber(1.5, .4, 'DIVIDE')).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 1.5, .4)).to.equal('Error');
     });
     it('should equal Error', () => {
-      expect(calculateNumber(1.5, -.4, 'DIVIDE')).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 1.5, -.4)).to.equal('Error');
     });
     it('should equal -2', () => {
-      expect(calculateNumber(1.5, -.6, 'DIVIDE')).to.equal(-2);
+      expect(calculateNumber('DIVIDE', 1.5, -.6)).to.equal(-2);
     });
     it('should equal Error', () => {
-      expect(calculateNumber(1.5, .4, 'DIVIDE')).to.equal('Error');
+      expect(calculateNumber('DIVIDE', 1.5, .4)).to.equal('Error');
     });
   });
 });
