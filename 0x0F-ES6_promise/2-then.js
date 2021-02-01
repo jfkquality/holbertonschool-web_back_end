8a8169acc8b85ce076ed6d60401fe259fa3900dc
jfkquality <jfkquality@gmail.com>
2021-02-01 00:11:43 -0800

    578. [bugfix]. Get correct order of then, finally.

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => (
+  return promise.then(() => (
     {
       status: 200,
       body: 'Success',
-    }, () => (Error())
-  ));
-  promise.finally(() => {
-    console.log('Got a response from the API');
-  });
+    }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }
