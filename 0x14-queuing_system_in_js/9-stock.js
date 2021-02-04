70b879a0a175889ddd21cd8af191e8db1be234fe
jfkquality <jfkquality@gmail.com>
2021-02-03 20:54:53 -0800

    620. t-12. [Feature]. Change object key names to match output.

--- 0x14-queuing_system_in_js/9-stock.js
+++ 0x14-queuing_system_in_js/9-stock.js
@@ -2,20 +2,24 @@ import express from 'express';
 
 
 const listProducts = [
-  {Id: 1, name: Suitcase 250, price: 50, stock: 4},
-  {Id: 2, name: Suitcase 450, price: 100, stock: 10},
-  {Id: 3, name: Suitcase 650, price: 350, stock: 2},
-  {Id: 4, name: Suitcase 1050, price: 550, stock: 5},
+  {ItemId: 1, itemName: Suitcase 250, price: 50, initialAvailableQuantity: 4},
+  {ItemId: 2, itemName: Suitcase 450, price: 100, initialAvailableQuantity: 10},
+  {ItemId: 3, itemName: Suitcase 650, price: 350, initialAvailableQuantity: 2},
+  {ItemId: 4, itemName: Suitcase 1050, price: 550, initialAvailableQuantity: 5},
 ];
 
 function getItemById(id) {
   const obj = listProducts.find(obj => obj.id == id);
+  return obj;
 }
 const app = express();
 const port = 1245;
 
 app.use(express.json());
 
+app.get('/list_products', (req, res) => {
+  res.json(listProducts);
+}
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
