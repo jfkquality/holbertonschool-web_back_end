5aa9f02526d6a75419d71089c09f57684288d016
jfkquality <jfkquality@gmail.com>
2021-02-04 16:32:04 -0500

    620. t-12. [Feature]. Try to get reserve stock to work.

--- 0x14-queuing_system_in_js/9-stock.js
+++ 0x14-queuing_system_in_js/9-stock.js
@@ -4,20 +4,26 @@ import { promisify } from 'util';
 
 
 const listProducts = [
-  {ItemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 4},
-  {ItemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10},
-  {ItemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2},
-  {ItemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5},
+  {itemId: 1, itemName: 'Suitcase 250', price: 50, initialAvailableQuantity: 0},
+  {itemId: 2, itemName: 'Suitcase 450', price: 100, initialAvailableQuantity: 10},
+  {itemId: 3, itemName: 'Suitcase 650', price: 350, initialAvailableQuantity: 2},
+  {itemId: 4, itemName: 'Suitcase 1050', price: 550, initialAvailableQuantity: 5},
 ];
 
 function getItemById(id) {
-  const obj = listProducts.find(obj => obj.iteimId == id);
+  const obj = listProducts.find(obj => obj.itemId == id);
+  // console.log('OBJ FROM GET ITEM BY ID: ', obj, 'ITEM ID: ', id);
   return obj;
 }
+
+// Create express server
 const app = express();
 const port = 1245;
 
 app.listen(port, () => {
+  for (const product of listProducts) {
+    reserveStockById(product.itemId, product.initialAvailableQuantity)
+  }
   console.log(`Server running on port ${port}`);
 });
 
@@ -27,34 +33,52 @@ app.get('/list_products', (req, res) => {
   res.json(listProducts);
 });
 
-
+// Create redis client
 const client = redis.createClient();
 client.on('error', (error) => {
       console.error(error);
 });
+const clientGet = promisify(client.get).bind(client);
 
 function reserveStockById(itemId, stock) {
-  client.set(itemId, stock)
+  client.set(itemId, stock) //currentQuantity
 }
 
 async function getCurrentReservedStockById(itemId) {
-  return client.get(itemId);
+  const item = await clientGet(itemId)
+  // console.log('CURRENT AVAIL STOCK: ',  item.currentQuantity);
+  return item.currentQuantity;
 }
 
 app.get('/list_products/:itemId', (req, res) => {
-  if (!getItemById(req.params.itemId)) {
-    res.json({'status': 'Product not found'});
-  }
-});
+  const itemId = req.params.itemId;
+  const item = getItemById(itemId);
+  const stock = getCurrentReservedStockById(itemId);
 
-app.get('/reserve_product/:itemId', (req, res) => {
-  const item = getItemById(req.params.itemId);
+  // console.log('ITEM FROM APP.GET LIST PROD BY ID: ', item);
   if (!item) {
     res.json({'status': 'Product not found'});
   }
-  if (item.initialAvailableQuantity < 1) {
-    res.json({"status":"Reservation confirmed","itemId": item.itemId});
+  item.currentQuantity = stock;
+  res.json(item);
+});
+
+app.get('/reserve_product/:itemId', (req, res) => {
+  const itemId = req.params.itemId;
+  const item = getItemById(itemId);
+
+  if (!item) {
+    res.json({'status': 'Product not found'});
+    return;
   }
+  const stock = getCurrentReservedStockById(itemId);
+
+  if (stock < 1) {
+    res.json({"status":"Not enough stock available","itemId": itemId});
+    return;
+  }
+  reserveStockById(itemId, stock);
+  res.json({"status":"Reservation confirmed","itemId": item.itemId});
 });
 
 export default app;
