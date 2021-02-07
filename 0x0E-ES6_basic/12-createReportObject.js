ce8fc45148e474c372826016fdbc4cad8bff9d3d
jfkquality <jfkquality@gmail.com>
2021-02-07 16:46:42 -0500

    577. T.13. [Bugfix]. Linter err.

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList,  // {...employeesList} or employessList
+    allEmployees: employeesList, // {...employeesList} or employessList
     getNumberOfDepartments(employeesList) {
       return (Object.keys(employeesList).length);
     },
