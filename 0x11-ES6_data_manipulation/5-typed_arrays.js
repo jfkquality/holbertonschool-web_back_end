73108659c7b66d0efdbb54d9736461c9428007cb
jfkquality <jfkquality@gmail.com>
2021-02-06 23:28:51 -0500

    580. T.5. [Bugfix] Add test for position out of range.

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,4 +1,7 @@
 export default function createInt8TypedArray(length, position, value) {
+  if (position >= length) {
+    throw new Error('Position outside range');
+  }
   const arrBuff = new ArrayBuffer(length);
   const int8 = new Int8Array(arrBuff, 0, length);
   int8.set([value], position);
