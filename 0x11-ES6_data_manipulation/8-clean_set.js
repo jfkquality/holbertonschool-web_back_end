bd737118a6c2bba7a7c199efbb629a4b81592d90
jfkquality <jfkquality@gmail.com>
2021-02-07 01:01:59 -0500

    580. T.8. [Bugfix]. Handle bad start string, empty set values.

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return '';
-  const arr = Array.from(set).filter(
-    (a) => a.includes(startString),
-  ).map((e) => e.slice(startString.length)).join('-');
-  return (arr);
+  if (!startString || typeof startString !== 'string') return '';
+  const arr = Array.from(set); // .filter(
+  //   (a) => a.includes(startString),
+  // ).map((e) => e.slice(startString.length)).join('-');
+
+  const arr2 = arr.filter((a) => (a ? a.includes(startString) : ''));
+  const arr3 = arr2.map((e) => e.slice(startString.length));
+  return (arr3.join('-'));
 }
