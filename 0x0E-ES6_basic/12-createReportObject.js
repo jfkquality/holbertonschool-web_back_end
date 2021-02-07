d28815c42322735ce3d8f65bd46842218cc3fc95
jfkquality <jfkquality@gmail.com>
2021-02-07 16:43:59 -0500

    577. T.13. Return array of employees not depts. Made t-14 fail.

--- 0x0E-ES6_basic/12-createReportObject.js
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: employeesList, // or  {...employeesList},
+    allEmployees: employeesList,  // {...employeesList} or employessList
     getNumberOfDepartments(employeesList) {
       return (Object.keys(employeesList).length);
     },
