ef8cf2dd35da41fbaf7e7853123ad7c935dde004
jfkquality <jfkquality@gmail.com>
2021-02-06 23:26:21 -0500

    580. T.5. [Init commit].

---
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,9 @@
+export default function createInt8TypedArray(length, position, value) {
+  const arrBuff = new ArrayBuffer(length);
+  const int8 = new Int8Array(arrBuff, 0, length);
+  int8.set([value], position);
+  const dataView = new DataView(arrBuff); // , 0, length);
+  // dataView.setInt8 = (position, value);
+  // const vectorLength = dataView.getUint8(position);
+  return dataView;
+}
