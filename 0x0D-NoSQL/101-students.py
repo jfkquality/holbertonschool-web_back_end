d71f30599250da10dc452aafbc9deb4656d332ed
jfkquality <jfkquality@gmail.com>
2021-02-08 02:08:13 -0800

    411. T.14/101. [Refactor]. Use loops and other functions vs aggregate.

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -4,24 +4,34 @@
 
 def top_students(mongo_collection):
     """ returns all students sorted by average score """
-    avg_score = mongo_collection.aggregate([
-        {"$unwind": "$topics"},
-        {"$group":
-         {
-             "_id": "$name",
-             "averageScore": {"$avg": "$topics.score"}
-         }},
-        {"$sort": {"averageScore": -1}},
-        {"$project":
-         {
-             "_id": 1,
-             "name": 1,
-             "averageScore": 1
-         }}
-    ])
+    students = [x for x in mongo_collection.find()]
+    for student in students:
+        scores = [x.get('score', 0) for x in student.get('topics', [])]
+        avg = 0
+        for score in scores:
+            avg = avg + score
+        avg = avg / len(scores)
+        student["averageScore"] = avg
+    return sorted(students, key=lambda z: z["averageScore"], reverse=True)
 
-    # , function(err, out) { res.send(out) }
+    # avg_score = mongo_collection.aggregate([
+    #     {"$unwind": "$topics"},
+    #     {"$group":
+    #      {
+    #          "_id": "$name",
+    #          "averageScore": {"$avg": "$topics.score"}
+    #      }},
+    #     {"$sort": {"averageScore": -1}},
+    #     {"$project":
+    #      {
+    #          "_id": 1,
+    #          "name": 1,
+    #          "averageScore": 1
+    #      }}
+    # ])
 
-    # for i in avg_score:
-    #     print(i)
-    return avg_score
+    # # , function(err, out) { res.send(out) }
+
+    # # for i in avg_score:
+    # #     print(i)
+    # return avg_score
