98510122c13224623d9540591816389a98d8714d
jfkquality <jfkquality@gmail.com>
2021-02-07 23:41:09 -0500

    578. T.3. [Bugfix]. Linter errors .

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
   return Promise.all([photo, user]).then((results) => {
-    console.log(`${ results[0].body } ${ results[1].firstName } ${ results[1].lastName }`);
+    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
   }).catch(() => new Error('Signup system offline'));
 }
 
