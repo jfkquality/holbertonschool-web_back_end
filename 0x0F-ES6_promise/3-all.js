d919b31160bfca5e7ab0cb221528ef05e651f432
jfkquality <jfkquality@gmail.com>
2021-02-07 23:23:45 -0500

    578. T.3. [Bugfix]. Return the promise (not in instructions; only say to console log.

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
-  Promise.all([photo, user]).then((results) => {
+  return Promise.all([photo, user]).then((results) => {
     console.log(results[0].body, results[1].firstName, results[1].lastName);
   });
 }
