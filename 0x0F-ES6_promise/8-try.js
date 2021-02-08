458f1d4e53e505aaef4e4f250c261d3da586f4bf
jfkquality <jfkquality@gmail.com>
2021-02-08 17:07:06 -0500

    578. t.8. [Bugfix/Refactor?]. Replace one line catch(err) w catch block

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -2,5 +2,8 @@ export default function divideFunction(numerator, denominator) {
   try {
     if (denominator === 0) throw new Error('cannot divide by 0');
     return numerator / denominator;
-  } catch (err) { return err; }
+  } catch (err) {
+    // err.mesage ='cannot divide by 0';
+    return (err);
+  }
 }
