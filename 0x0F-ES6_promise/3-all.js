bb50a23dc57b5a1fbbfe1bf36c6fc7574424aa2b
jfkquality <jfkquality@gmail.com>
2021-02-07 23:34:36 -0500

    578. T.3. [Bugfix]. Add catch for errors.

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -5,7 +5,7 @@ export default function handleProfileSignup() {
   const user = createUser();
   return Promise.all([photo, user]).then((results) => {
     console.log(results[0].body, results[1].firstName, results[1].lastName);
-  });
+  }).catch(() => new Error('Signup system offline'));
 }
 
 // handleProfileSignup();
