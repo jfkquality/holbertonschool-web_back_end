da88b47839b3bf08684fcaeb052623f59375f431
jfkquality <jfkquality@gmail.com>
2021-02-07 23:47:08 -0500

    578. T.3. [Bugfix]. Fix error console.log.

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -4,8 +4,9 @@ export default function handleProfileSignup() {
   const photo = uploadPhoto();
   const user = createUser();
   return Promise.all([photo, user]).then((results) => {
-    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`);
-  }).catch(() => new Error('Signup system offline'));
+    console.log(`${results[0].body} ${results[1].firstName} ${results[1].lastName}`),
+    () => console.log(`Signup system offline`)
+  });
 }
 
 // handleProfileSignup();
