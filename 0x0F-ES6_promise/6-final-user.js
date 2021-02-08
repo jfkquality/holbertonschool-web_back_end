b7150f0c6620d88bdf913039d23be702b75608cd
jfkquality <jfkquality@gmail.com>
2021-02-08 06:15:20 -0500

    578. t.6. [Bugfix]. Return result object CORRECTLY.

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -2,9 +2,10 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [signUpUser(fileName), uploadPhoto(firstName, lastName)];
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName, lastName)];
+  // console.log(promises);
   return Promise.allSettled(promises)
-    .then((results) => results.forEach((result) => { result.status, result.value }));
+    .then((result) => result);
 }
 
 console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
