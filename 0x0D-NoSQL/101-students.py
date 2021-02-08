d8dcdbf67ec26ad16e2d58e5cc3c54019cb381b7
jfkquality <jfkquality@gmail.com>
2021-02-08 02:03:20 -0800

    411. T.14/101. [Init commit]. Incorrect output but correct data and sort.

--- 0x0D-NoSQL/101-students.py
+++ 0x0D-NoSQL/101-students.py
@@ -4,6 +4,24 @@
 
 def top_students(mongo_collection):
     """ returns all students sorted by average score """
-    return mongo_collection.aggregate({
-        "$group": {"avg_score": {avg: core}}
-    })
+    avg_score = mongo_collection.aggregate([
+        {"$unwind": "$topics"},
+        {"$group":
+         {
+             "_id": "$name",
+             "averageScore": {"$avg": "$topics.score"}
+         }},
+        {"$sort": {"averageScore": -1}},
+        {"$project":
+         {
+             "_id": 1,
+             "name": 1,
+             "averageScore": 1
+         }}
+    ])
+
+    # , function(err, out) { res.send(out) }
+
+    # for i in avg_score:
+    #     print(i)
+    return avg_score
